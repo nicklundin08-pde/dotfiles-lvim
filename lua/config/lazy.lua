b3787fe31f0bcac2a9a7d8e3f8806c235b8b4e38
Nick Lundin <nicholas.lundin@betterlesson.com>
2024-04-12 08:21:41 -0500

    Java extra with lombok

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -5,11 +5,11 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
-
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.java" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
